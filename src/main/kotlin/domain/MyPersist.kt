37b5b38ea6d56439a539b7c0d72a6d6e7f3325b0
Tim <tim.abilzade@gmail.com>
2018-08-18 14:35:31 -0500

    Add my own Graph layout persistance to workaround Jung bug.

---
+++ src/main/kotlin/domain/MyPersist.kt
@@ -0,0 +1,34 @@
+package domain
+
+import edu.uci.ics.jung.algorithms.layout.Layout
+import edu.uci.ics.jung.visualization.layout.PersistentLayout
+import edu.uci.ics.jung.visualization.layout.PersistentLayoutImpl
+import java.io.*
+
+class MyPersist<T, Y>(layout: Layout<T, Y>) : PersistentLayoutImpl<T, Y>(layout) {
+
+    fun persistJ(fileName: String) {
+
+        locations = locations.toMutableMap()
+        graph.vertices.forEach { v ->
+            locations[v] = PersistentLayout.Point(transform(v))
+        }
+        initializeLocations()
+
+        val oos = ObjectOutputStream(FileOutputStream(fileName))
+        oos.writeObject(locations)
+        oos.close()
+    }
+
+    override fun restore(fileName: String) {
+        val ois = ObjectInputStream(FileInputStream(fileName))
+        //locations = locations.toMutableMap()
+        val readObject = ois.readObject()
+        print("")
+        locations = readObject as Map<T, PersistentLayout.Point>
+        ois.close()
+        locked = true
+        fireStateChanged()
+        initializeLocations()
+    }
+}
